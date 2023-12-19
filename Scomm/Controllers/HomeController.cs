c07912c4ae6bad2916ce2f7dc92d51b3bddfc7bb
Abdullah Zaber <bond2164@gmail.com>
2023-12-19 22:00:12 +0600

    Remove ignored files

--- Scomm/Controllers/HomeController.cs
+++ Scomm/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DAL;
+using DAL.EntityModel;
+using DAL.UnitOfWork;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Scomm.Data;
 using Scomm.Models;
 using System.Diagnostics;
 
@@ -8,27 +12,76 @@ namespace Scomm.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _context = context;
+            _unitOfWork = unitOfWork;
         }
 
+        [HttpGet]
         public IActionResult Item()
         {
             return View();
         }
 
         [Authorize(Roles = "Admin")]
+        [HttpGet]
         public IActionResult Category()
         {
-            return View();
+            //IEnumerable<Category> model = _context.Category.Select(s => new Category
+            //{
+            //    ID = s.ID,
+            //    CategoryName = s.CategoryName
+            //});
+            IEnumerable<Category> model = _unitOfWork.Categories.GetCategories();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            var Category = new Category
+            {
+                CategoryName = category.CategoryName
+            };
+            _unitOfWork.Categories.Add(Category);
+            _unitOfWork.Complete();
+            return Ok();
+        }
+        public PartialViewResult AddCategoryPartialView()
+        {
+            return PartialView("_categoryAdd", new Category());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet("Home/RemoveCategory/{id:int}")]
+        public ActionResult<Category> RemoveCategory(int id)
+        {
+            try
+            {
+                var Category = _unitOfWork.Categories.GetById(id);
+
+                if (Category == null)
+                {
+                    return NotFound($"Category with Id = {id} not found");
+                }
+                _unitOfWork.Categories.Remove(Category);
+                _unitOfWork.Complete();
+                return RedirectToAction("Category", "Home");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error deleting data");
+            }
         }
     }
 }
