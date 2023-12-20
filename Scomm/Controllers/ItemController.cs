b59c7e5110b62781d7ad42d56b232b9d3815e773
Abdullah Zaber <bond2164@gmail.com>
2023-12-20 15:46:43 +0600

    upload & read excel file. display it on the view

---
+++ Scomm/Controllers/ItemController.cs
@@ -0,0 +1,63 @@
+﻿using ExcelDataReader;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scomm.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UploadItemExcel()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadItemExcel(IFormFile file)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            //Upload the excel file
+            if (file != null && file.Length > 0)
+            {
+                var uploadDirectory = $"{Directory.GetCurrentDirectory()}\\wwwroot\\Uploads";
+                if (!Directory.Exists(uploadDirectory))
+                {
+                    Directory.CreateDirectory(uploadDirectory);
+                }
+                var filePath = Path.Combine(uploadDirectory, file.FileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                //Read file
+                using (var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    // Auto-detect format, supports:
+                    //  - Binary Excel files (2.0-2003 format; *.xls)
+                    //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+                    var excelData = new List<List<object>>();
+                    using (var reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        // 1. Use the reader methods
+                        do
+                        {
+                            while (reader.Read())
+                            {
+                                // reader.GetDouble(0);
+                                var rowData = new List<object>();
+                                for (int column = 0; column < reader.FieldCount; column++)
+                                {
+                                    rowData.Add(reader.GetValue(column));
+                                }
+                                excelData.Add(rowData);
+                            }
+                        } while (reader.NextResult());
+                        ViewBag.excelData = excelData;
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
